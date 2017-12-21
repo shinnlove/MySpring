33ee49cc71a640f819c5d085199e72e849b92fdb
chensheng.zcs <chensheng.zcs@alipay.com>
2017-12-21 17:29:24 +0800

    配置日志门面

--- src/main/java/com/shinnlove/common/util/logtest/TestLog.java
+++ src/main/java/com/shinnlove/common/util/logtest/TestLog.java
@@ -4,8 +4,11 @@
  */
 package com.shinnlove.common.util.logtest;
 
+import java.io.FileNotFoundException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.Log4jConfigurer;
 
 /**
  * 测试日志打印
@@ -16,6 +19,14 @@ import org.slf4j.LoggerFactory;
 public class TestLog {
 
     private static Logger logger = LoggerFactory.getLogger(TestLog.class);
+
+    static {
+        try {
+            Log4jConfigurer.initLogging("classpath:META-INF/log/log4j.properties");
+        } catch (FileNotFoundException ex) {
+            System.err.println("Cannot Initialize log4j");
+        }
+    }
 
     public static void main(String[] args) {
         logger.info("测试slf4j打印log4j类型日志");
