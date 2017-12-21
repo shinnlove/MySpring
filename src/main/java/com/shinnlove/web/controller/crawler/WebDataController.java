5f91676121dcc9be7977b5fc0a3f842d63fdf0d0
chensheng.zcs <chensheng.zcs@alipay.com>
2017-12-21 21:24:45 +0800

    修改Log4j的日志门面，不再直接使用Log4j。

--- src/main/java/com/shinnlove/web/controller/crawler/WebDataController.java
+++ src/main/java/com/shinnlove/web/controller/crawler/WebDataController.java
@@ -6,7 +6,8 @@ package com.shinnlove.web.controller.crawler;
 
 import java.util.List;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +32,7 @@ import com.shinnlove.web.controller.request.WebDataRequest;
 public class WebDataController {
 
     /** 日志 */
-    private static Logger logger = Logger.getLogger(WebDataController.class);
+    private static Logger logger = LoggerFactory.getLogger(WebDataController.class);
 
     @Autowired
     private WebDataDao webDataDao;
