ed9acee03575deca63a091b423bab9bc6afbaab8
chensheng.zcs <chensheng.zcs@alipay.com>
2018-01-01 11:29:32 +0800

    配置spring自带的schedule任务。

---
+++ src/main/java/com/shinnlove/core/service/interval/task/MyAnnotationTask.java
@@ -0,0 +1,22 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package com.shinnlove.core.service.interval.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * 验证被注解扫描的定时任务。
+ *
+ * @author shinnlove.jinsheng
+ * @version $Id: MyAnnotationTask.java, v 0.1 2018-01-01 上午11:18 shinnlove.jinsheng Exp $$
+ */
+public class MyAnnotationTask {
+
+    @Scheduled(cron = "* * * * * ?")
+    public void annotationShowTask() {
+        System.out.println("这是被注解扫描到的任务！");
+    }
+
+}
---
+++ src/main/java/com/shinnlove/core/service/interval/task/MyIntervalTask.java
@@ -0,0 +1,19 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package com.shinnlove.core.service.interval.task;
+
+/**
+ * 给spring自带task用的定时任务类。
+ *
+ * @author shinnlove.jinsheng
+ * @version $Id: MyIntervalTask.java, v 0.1 2018-01-01 上午11:13 shinnlove.jinsheng Exp $$
+ */
+public class MyIntervalTask {
+
+    public void showTask() {
+        System.out.println("这是我的task，期望被定时调度！");
+    }
+
+}
