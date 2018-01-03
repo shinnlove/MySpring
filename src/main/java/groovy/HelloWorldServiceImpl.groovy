f93596e4a52bb5e28da1cf0feba50a04b06bf72f
chensheng.zcs <chensheng.zcs@alipay.com>
2018-01-03 18:05:47 +0800

    提交spring集成groovy代码。

--- src/main/java/groovy/HelloWorldServiceImpl.groovy
+++ src/main/java/groovy/HelloWorldServiceImpl.groovy
@@ -2,7 +2,7 @@
  * Alipay.com Inc.
  * Copyright (c) 2004-2018 All Rights Reserved.
  */
-package com.shinnlove.common.service.integration.impl;
+package groovy;
 
 import com.shinnlove.common.service.integration.HelloWorldService;
 
